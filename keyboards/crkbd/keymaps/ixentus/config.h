aaa12cda09ac6b48d1e8bacbe3cb7bc4f9c983f0
Acid <alexvermaat@gmail.com>
2024-01-16 11:05:37 +0100

    yes

--- keyboards/crkbd/keymaps/ixentus/config.h
+++ keyboards/crkbd/keymaps/ixentus/config.h
@@ -30,7 +30,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 400
-#define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
 #define SPLIT_LAYER_STATE_ENABLE
 
