fc4616279b4965c587af87c02e505e5ceb6f2efa
Acid <alexvermaat@gmail.com>
2024-01-16 10:41:44 +0100

    Update rules for oled driver

--- keyboards/crkbd/keymaps/ixentus/rules.mk
+++ keyboards/crkbd/keymaps/ixentus/rules.mk
@@ -2,7 +2,6 @@ BOOTLOADER = atmel-dfu
 MOUSEKEY_ENABLE = yes    # Mouse keys
 RGB_MATRIX_ENABLE = yes    # Enable WS2812 RGB underlight.
 OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
 CAPS_WORD_ENABLE = yes
 WPM_ENABLE	= yes
